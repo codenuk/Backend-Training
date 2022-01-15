a28c68f6065639dfa4dc7772fc61037c970336f8
codenuk <supagorn00001@gmail.com>
2022-01-15 19:55:18 +0700

    update

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const Firstname = 'Supagorn'
-const hello = (name) => {
-    return `Hello ${name}`
-}
-console.log(hello(Firstname))
-
-
-
-const sum = (a, b) => a + b
-module.exports = sum
-
+const express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.json({ message: 'Ahoy!' })
+})
+app.listen(4000, () => {
+  console.log('Application is running on port 4000')
+})
--- main.js
+++
@@ -1,3 +0,0 @@
-const sum = require('./index.js')
-const result = sum(10, 20)
-console.log('main.js > ', result)
